d191738b9cc89321ee6848929eeac3e7d23449d1
tanishq0917t <tanishqrawat8@gmail.com>
2024-11-02 15:44:54 +0530

    Adding model for vps_details

--- Django_Based/dashboard/models.py
+++ Django_Based/dashboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 
+class vps_details(models.Model):
+    name=models.CharField()
+    storage=models.IntegerField()
+    os=models.CharField()
+    type=models.CharField()
+    user=models.CharField()
+
+
