8b9bf2d18125e2786b4f4715211f44fd0a69190f
tanishq0917t <tanishqrawat8@gmail.com>
2024-11-06 15:32:24 +0530

    Adding create EC2 code, needs to be modified

---
+++ Django_Based/dashboard/tasks.py
@@ -0,0 +1,58 @@
+from celery import shared_task
+import boto3
+import os
+
+
+
+#Below code has to be modified as per Django's configurations. Currently it is copied from the flask folder
+@shared_task
+def create_EC2(data):
+    aws_access_key_id=os.getenv('ACCESS_KEY')
+    aws_secret_access_key=os.getenv('SECRET_KEY')
+    region = 'ap-south-1'
+    instance_type=None
+    ami_id=None
+    if data['serverType']=="micro": instance_type = 't2.micro'
+    ami_id = data['serverOS']
+    key_name = f"{data['serverName']}-key-pair"
+    security_group_ids = ['launch-wizard-1']
+    volume_size = int(data['serverStorage'])
+    ec2 = boto3.client('ec2', region_name=region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+    block_device_mappings = [{
+            'DeviceName': '/dev/xvda',
+            'Ebs': {
+                'VolumeSize': volume_size,
+                'VolumeType': 'gp2'
+            }
+        }]
+    key_pair_response = ec2.create_key_pair(KeyName=key_name)
+    pem_file_path = f"pems/{data['user'].split('@')[0]}/{key_name}.pem"
+    with open(pem_file_path, "w") as pem_file:
+        pem_file.write(key_pair_response['KeyMaterial'])
+    instance_response = ec2.run_instances(
+        ImageId=ami_id,
+        InstanceType=instance_type,
+        KeyName=key_name,
+        SecurityGroupIds=security_group_ids,
+        MinCount=1,
+        MaxCount=1,
+        BlockDeviceMappings=block_device_mappings,
+        TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': data['serverName']
+                        },
+                    ]
+                }
+            ]
+        )
+    instance_id = instance_response['Instances'][0]['InstanceId']
+    lst=[instance_id]
+    ec2.get_waiter('instance_running').wait(InstanceIds=lst)
+    instance_info = ec2.describe_instances(InstanceIds=lst)
+    instance = instance_info['Reservations'][0]['Instances'][0]
+    public_ip = instance.get('PublicIpAddress')
+    return (instance_id,public_ip)
--- Django_Based/dashboard/views.py
+++ Django_Based/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+import os
 
 def dashboard(request):
     return render(request,'dashboard/dashboard.html',{'user':request.session.get('user')})
@@ -14,6 +15,6 @@ def configureNewVPS(request):
     if request.method=="POST":
         print(request.POST.getlist('list[]'))
         #Write the code to fetch form and initiate a celery task
-        return JsonResponse({'status':'success','message':'Done'})
+        return JsonResponse({'status':os.getenv('ACCESS_KEY'),'message':os.getenv('SECRET_KEY')})
     else:
         return JsonResponse({'status':'error','message':'Not Done'})
